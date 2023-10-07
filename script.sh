7d292f15c4b63ab58626a4c3e96dd644a2303e0b
reyfull <111069621+reyfull@users.noreply.github.com>
2023-10-07 23:55:10 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -19,7 +19,7 @@ if grep -q "emby-crx" index.html; then
     echo "Index.html already contains emby-crx, skipping insertion."
 else
     # 定义要插入的代码
-    code='<link rel="stylesheet" id="theme-css" href="emby-crx/style.css" type="text/css" media="all" />\n<script src="emby-crx/common-utils.js"></script>\n<script src="emby-crx/jquery-3.6.0.min.js"></script>\n<script src="emby-crx/md5.min.js"></script>\n<script src="emby-crx/config.js"></script>\n<script src="emby-crx/main.js"></script>'
+    code='<link rel="stylesheet" id="theme-css" href="emby-crx/style.css" type="text/css" media="all" />\n<script src="emby-crx/common-utils.js"></script>\n<script src="emby-crx/jquery-3.6.0.min.js"></script>\n<script src="emby-crx/md5.min.js"></script>\n<script src="emby-crx/config.js"></script>\n<script src="emby-crx/main.js"></script>\n<script src="emby-crx/embyLaunchPotplayer.js"></script>'
 
     # 在</head>之前插入代码
     new_content=$(echo -e "${content/<\/head>/$code<\/head>}")
