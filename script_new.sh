77925655e3793b90b904e86179cc18b6f5e0ef7a
reyfull <111069621+reyfull@users.noreply.github.com>
2023-11-03 12:32:16 +0800

    Update script_new.sh

--- script_new.sh
+++ script_new.sh
@@ -24,6 +24,7 @@ wget https://raw.githubusercontent.com/reyfull/emby-crx/master/icon-PotPlayer.we
 wget https://raw.githubusercontent.com/reyfull/emby-crx/master/icon-StellarPlayer.webp -P emby-crx/
 wget https://raw.githubusercontent.com/reyfull/emby-crx/master/icon-VLC.webp -P emby-crx/
 wget https://raw.githubusercontent.com/reyfull/emby-crx/master/ede.user.js -P emby-crx/
+wget https://raw.githubusercontent.com/reyfull/emby-crx/master/danmaku.min.js -P emby-crx/
 # 读取index.html文件内容
 content=$(cat index.html)
 
@@ -32,7 +33,7 @@ if grep -q "emby-crx" index.html; then
     echo "Index.html already contains emby-crx, skipping insertion."
 else
     # 定义要插入的代码
-    code='<link rel="stylesheet" id="theme-css" href="emby-crx/style.css" type="text/css" media="all" />\n<script src="emby-crx/common-utils.js"></script>\n<script src="emby-crx/jquery-3.6.0.min.js"></script>\n<script src="emby-crx/md5.min.js"></script>\n<script src="emby-crx/config.js"></script>\n<script src="emby-crx/main.js"></script>\n<script src="emby-crx/embyLaunchPotplayer.js">\n<script src="emby-crx/ede.user.js"></script>'
+    code='<link rel="stylesheet" id="theme-css" href="emby-crx/style.css" type="text/css" media="all" />\n<script src="emby-crx/common-utils.js"></script>\n<script src="emby-crx/jquery-3.6.0.min.js"></script>\n<script src="emby-crx/md5.min.js"></script>\n<script src="emby-crx/config.js"></script>\n<script src="emby-crx/main.js"></script>\n<script src="emby-crx/embyLaunchPotplayer.js">\n<script src="emby-crx/ede.user.js">\n<script src="emby-crx/danmaku.min.js"></script>'
 
     # 在</head>之前插入代码
     new_content=$(echo -e "${content/<\/head>/$code<\/head>}")
